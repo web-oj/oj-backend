85e7a56567e592bff1a6ee5f2994f42bca2da7a0
Tranduy1dol <tmd.iid2004@gmail.com>
2024-11-01 16:53:54 +0700

    feat: add tag api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import user from "./user/user";
 import problem from "./problem/problem";
+import tag from "./tag/tag";
 
 const router = Router();
 
 router.use("/user", user);
 router.use("/problem", problem);
+router.use("/tag", tag);
 
 export default router;
