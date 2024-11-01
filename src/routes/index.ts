91f9ddf880a6e779070fb04c1c4e1195289a6bd0
Tranduy1dol <tmd.iid2004@gmail.com>
2024-11-01 16:34:10 +0700

    feat: add problem api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
-import {Router} from "express"
+import { Router } from "express";
 import user from "./user/user";
+import problem from "./problem/problem";
 
+const router = Router();
 
-const router = Router()
+router.use("/user", user);
+router.use("/problem", problem);
 
-router.use('/user', user)
-
-export default router
+export default router;
