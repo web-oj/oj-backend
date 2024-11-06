b4f6b9ef9d728027909316f17dc4f9e5477516de
Tranduy1dol <tmd.iid2004@gmail.com>
2024-11-06 14:24:41 +0700

    feat(route): refactor user route

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -2,21 +2,19 @@
  * @file user.ts
  * @summary User routes
  * @description Handles following routes:
- *      - GET '/'
+ *      - GET '/:handle'
  *      - GET '/:id'
  *      - DELETE '/:id'
  *      - PATCH ':/id'
- *      - PATCH '/role/:id'
- *      - POST '/'
+ *      - PATCH '/admin/:id'
+ *      - POST '/createUser'
  *      - POST '/login'
- *      - POST '/logout'
  */
 
-import { Request, Response, Router } from "express";
-import { UserController } from "../controllers/userController";
-import { auth } from "../middleware/auth";
-import { UserService } from "../services/impl/UserService";
-import { Get } from "tsoa";
+import { NextFunction, Request, Response, Router } from "express";
+import { UserController } from "@/controllers/userController";
+import { UserService } from "@/services/impl/UserService";
+import { auth } from "@/middleware/auth";
 
 const userService = new UserService();
 const userController = new UserController(userService);
@@ -30,6 +28,23 @@ router.post("/createUser", (req, res) => {
     res.status(200).send("User created successfully");
   } catch (err) {
     res.status(500).send(`Error creating user: ${err}`);
+  }
+});
+
+router.post("/login", async (req: Request, res: Response) => {
+  const { password, email } = req.body;
+  try {
+    const result = await userController.login({ password, email });
+    if (!result) {
+      res.status(404).send(`Invalid email or password`);
+    } else {
+      res.status(200).send({
+        message: "User logged in successfully",
+        token: result,
+      });
+    }
+  } catch (err) {
+    res.status(500).send(`Error login: ${err}`);
   }
 });
 
@@ -51,11 +66,85 @@ router.get("/:id", async (req: Request, res: Response) => {
         handle: user.handle,
         email: user.email,
       };
-      res.status(200).send(response);  
+      res.status(200).send(response);
     }
   } catch (err) {
     res.status(500).send(`Error getting user: ${err}`);
   }
 });
+
+router.get("/:handle", async (req: Request, res: Response) => {
+  const handle = req.params.handle;
+  try {
+    const user = await userController.getUserByHandle(handle);
+    if (!user) {
+      res.status(404).send(`User not found`);
+    } else {
+      const response: GetUserResponse = {
+        id: user.id,
+        handle: user.handle,
+        email: user.email,
+      };
+      res.status(200).send(response);
+    }
+  } catch (err) {
+    res.status(500).send(`Error getting user: ${err}`);
+  }
+});
+
+router.patch("/:id", async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const { handle, password, email } = req.body;
+  try {
+    const user = await userController.updateUser(id, {
+      handle,
+      password,
+      email,
+    });
+    if (!user) {
+      res.status(404).send(`User with id ${id} not found`);
+    } else {
+      res.status(200).send("User updated successfully");
+    }
+  } catch (err) {
+    res.status(500).send(`Error updating user: ${err}`);
+  }
+});
+
+router.patch(
+  "admin/:id",
+  (req: Request, res: Response, next: NextFunction) => {
+    auth(req, res, next, ["AD"]);
+  },
+  async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    try {
+      const user = userController.addAdmin(id);
+      if (!user) {
+        res.status(404).send(`User with not found`);
+      } else {
+        res.status(200).send("Admin added successfully");
+      }
+    } catch (err) {
+      res.status(500).send(`Error adding admin: ${err}`);
+    }
+  },
+);
+
+router.delete(
+  "/:id",
+  (req: Request, res: Response, next: NextFunction) => {
+    auth(req, res, next, ["AD"]);
+  },
+  async (req: Request, res: Response) => {
+    const id = req.params.id;
+    try {
+      await userController.deleteUser(id);
+      res.status(200).send("User deleted successfully");
+    } catch (err) {
+      res.status(500).send(`Error deleting user: ${err}`);
+    }
+  },
+);
 
 export default router;
