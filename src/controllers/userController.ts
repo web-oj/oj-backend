9c47f19e685dd9d0c477e29c045dac4377a2677c
Tranduy1dol <tmd.iid2004@gmail.com>
2024-11-06 14:24:21 +0700

    feat(controller): refactor user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -10,15 +10,16 @@
  *          - getUserByEmail()
  *          - getUserByHandle()
  *          - login()
- *          - logout()
- * @return Express JSON Response
+ * @return User information
  */
 
 import keccak256 from "keccak256";
 import "dotenv/config";
 import { IUserService } from "@/services/IUserService";
-import { Body, Get, Path, Post, Route } from "tsoa";
-import { User } from "../entities/User";
+import { Body, Delete, Get, Patch, Path, Post, Route } from "tsoa";
+import { User } from "@/entities/User";
+import { sign } from "jsonwebtoken";
+import { env } from "@/config/config";
 
 @Route("user")
 export class UserController {
@@ -30,40 +31,83 @@ export class UserController {
 
   @Post("createUser")
   public async createUser(
-    @Body() body: { handle: string; password: string; email: string }
+    @Body() body: { handle: string; password: string; email: string },
   ) {
     const { handle, password, email } = body;
     const hashedPassword = keccak256(password).toString();
-  
+
     try {
       this.userService.createUser({ handle, password: hashedPassword, email });
       return { message: "User created successfully" };
     } catch (err) {
       throw new Error(`Error creating user: ${err}`);
     }
-  }  
+  }
 
   @Get("{id}")
-  public async getUserById(
-    @Path() id: number
-  ): Promise<User | null> {
+  public async getUserById(@Path() id: number): Promise<User | null> {
     try {
-      const user = await this.userService.getUserById(id);
-      return user;
+      return await this.userService.getUserById(id);
     } catch (err) {
       throw new Error(`Error getting user: ${err}`);
     }
   }
 
   @Get("{handle}")
-  public async getUserByHandle(
-    @Path() handle: string
-  ): Promise<User | null> {
+  public async getUserByHandle(@Path() handle: string): Promise<User | null> {
     try {
-      const user = await this.userService.getUserByHandle(handle);
-      return user;
+      return await this.userService.getUserByHandle(handle);
     } catch (err) {
       throw new Error(`Error getting user: ${err}`);
+    }
+  }
+
+  @Delete("{id}")
+  public async deleteUser(@Path() id: string): Promise<void> {
+    try {
+      await this.userService.softDeleteUser(parseInt(id));
+    } catch (err) {
+      throw new Error(`Error delete user: ${err}`);
+    }
+  }
+
+  @Patch("{id}")
+  public async updateUser(
+    @Path() id: number,
+    @Body() body: { handle: string; password: string; email: string },
+  ) {
+    try {
+      return await this.userService.updateUser(id, body);
+    } catch (err) {
+      throw new Error(`Error updating user: ${err}`);
+    }
+  }
+
+  @Patch("admin/{id}")
+  public async addAdmin(@Path() id: number) {
+    try {
+      return await this.userService.updateUser(id, { role: "admin" });
+    } catch (err) {
+      throw new Error(`Error adding admin: ${err}`);
+    }
+  }
+
+  @Post("login")
+  public async login(@Body() body: { password: string; email: string }) {
+    try {
+      const { password, email } = body;
+      const user = await this.userService.getUserByEmail(email);
+
+      if (user) {
+        if (keccak256(password).toString("hex") === user.password) {
+          return sign(user, env.jwt_secret, {
+            expiresIn: "maxAge",
+          });
+        }
+      }
+      return null;
+    } catch (err) {
+      throw new Error(`Error login: ${err}`);
     }
   }
 }
